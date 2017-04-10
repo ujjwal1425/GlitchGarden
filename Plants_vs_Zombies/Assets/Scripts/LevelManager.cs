eeba01f8689f5de73ce46b79edabb156ed9d3e68
Uday <ujjwal.1425@gmail.com>
2017-04-10 22:36:29 +0530

    SplashScreen

---
+++ Plants_vs_Zombies/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+public float auotLoadNextLevel;
+	// Use this for initialization
+	void Start () {
+		Invoke("LoadNextLevel", auotLoadNextLevel);
+	}
+
+	public void LoadLevel (string name)
+	{
+	Application.LoadLevel(name);
+	}
+	public void QuitRequest ()
+	{
+	Application.Quit();
+	}
+	public void LoadNextLevel ()
+	{
+	Application.LoadLevel(Application.loadedLevel+1);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
